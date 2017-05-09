71009063616c041060d586022c039b3967250431
archibaldodelacruz <vimerinom@gmail.com>
2017-05-09 01:21:52 -0300

    Subida Inicial

---
+++ app/src/main/java/cl/archibaldo/floreantpos/MainActivity.java
@@ -0,0 +1,65 @@
+package cl.archibaldo.floreantpos;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.app.AlertDialog;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.content.DialogInterface;
+import android.content.Context;
+
+public class MainActivity extends Activity {
+
+    WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
+        this.setContentView(R.layout.activity_main);
+        myWebView = (WebView) this.findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.getSettings().setAllowContentAccess(true);
+        myWebView.getSettings().setAllowFileAccess(true);
+        myWebView.getSettings().setDatabaseEnabled(true);
+        myWebView.getSettings().setDomStorageEnabled(true);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setLoadWithOverviewMode(true);
+        myWebView.getSettings().setUseWideViewPort(false);
+
+        String url = "";
+
+
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+            alert.setTitle("Ingrese la IP");
+            alert.setMessage("Ingrese la IP de Floreant Pos en formato: ip:puerto");
+
+// Set an EditText view to get user input
+            final EditText input = new EditText(this);
+            alert.setView(input);
+
+            alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    String url = "http://" + input.getText().toString();
+                    myWebView.loadUrl(url);
+                }
+            });
+
+
+            alert.show();
+
+
+
+
+
+
+        /*myWebView.loadUrl("https://archibaldodelacruz.cl");*/
+    }
+
+}
