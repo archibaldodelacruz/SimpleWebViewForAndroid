dea034a4ed567f032e567a3d8c428a65782dbf8f
archibaldodelacruz <vimerinom@gmail.com>
2017-05-18 00:07:00 -0400

    Se esconde la barra de navegación automáticamente
    Se esconde la barra de navegación y la aplicación se ejecuta a pantalla
    completa.

--- app/src/main/java/cl/archibaldo/floreantpos/ActivityWebView.java
+++ app/src/main/java/cl/archibaldo/floreantpos/ActivityWebView.java
@@ -3,14 +3,15 @@ package cl.archibaldo.floreantpos;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-
 
 public class ActivityWebView extends Activity {
 
     WebView myWebView;
+    View decorView;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,24 +19,51 @@ public class ActivityWebView extends Activity {
         Intent intent = getIntent();
         String url = intent.getStringExtra("URL");
         this.setContentView(R.layout.activity_webview);
+
+        hideSystemUI();
+
         myWebView = (WebView) this.findViewById(R.id.webview);
-        myWebView.setWebViewClient(new WebViewClient());
-        myWebView.getSettings().setAllowContentAccess(true);
-        myWebView.getSettings().setAllowFileAccess(true);
-        myWebView.getSettings().setDatabaseEnabled(true);
-        myWebView.getSettings().setDomStorageEnabled(true);
         myWebView.getSettings().setJavaScriptEnabled(true);
         myWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
         myWebView.setSaveEnabled(true);
-
-        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         myWebView.getSettings().setLoadWithOverviewMode(true);
         myWebView.getSettings().setUseWideViewPort(true);
         myWebView.loadUrl(url);
+
+        decorView.setOnSystemUiVisibilityChangeListener
+                (new View.OnSystemUiVisibilityChangeListener() {
+                    @Override
+                    public void onSystemUiVisibilityChange(int visibility) {
+                        if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                            Handler espera3segundos = new Handler();
+                            espera3segundos.postDelayed(new Runnable(){
+                                public void run(){
+                                    hideSystemUI();
+                                }
+                            },3000);
+                        }
+                    }
+                });
+
+
     }
 
+    @Override
     protected void onPause() {
         super.onPause();
+    }
+
+    private void hideSystemUI() {
+
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        decorView = this.getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
 
     }
 
